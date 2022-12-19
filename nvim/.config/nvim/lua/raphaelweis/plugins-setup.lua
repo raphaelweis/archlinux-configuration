90aa0fdbcd7933e4a049a16d0262cceef154c93f
Raphël Weis <raphael.weis.2003@gmail.com>
2022-12-19 13:24:13 +0100

    finished basic neovim config

--- nvim/.config/nvim/lua/raphaelweis/plugins-setup.lua
+++ nvim/.config/nvim/lua/raphaelweis/plugins-setup.lua
@@ -1,13 +1,13 @@
 -- auto install packer if not installed
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-    vim.cmd([[packadd packer.nvim]])
-    return true
-  end
-  return false
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
 end
 local packer_bootstrap = ensure_packer() -- true if packer was just installed
 
@@ -23,83 +23,101 @@ vim.cmd([[
 -- Use a protected call so we don't error out on first use
 local status_OK, packer = pcall(require, "packer")
 if not status_OK then
-    return
+	return
 end
 
 -- Have packer use a popup window
-packer.init {
-    display = {
-        open_fn = function()
-            return require("packer.util").float { border = "rounded" }
-        end,
-    },
-}
+packer.init({
+	display = {
+		open_fn = function()
+			return require("packer.util").float({ border = "rounded" })
+		end,
+	},
+})
 
 -- Plugin installation
 return packer.startup(function(use)
-    -- Have packer manage itself
-    use("wbthomason/packer.nvim")
-    
-    -- lua fonctions that many plugins use
-    use("nvim-lua/popup.nvim")
-    use("nvim-lua/plenary.nvim")
+	-- Have packer manage itself
+	use("wbthomason/packer.nvim")
 
-    -- colorscheme
-    use("gruvbox-community/gruvbox")
+	-- lua fonctions that many plugins use
+	use("nvim-lua/popup.nvim")
+	use("nvim-lua/plenary.nvim")
 
-    -- tmux & split window navigation
-    use("christoomey/vim-tmux-navigator")
-    use("szw/vim-maximizer")
+	-- colorscheme
+	use("gruvbox-community/gruvbox")
 
-    -- essential plugins
-    use("tpope/vim-surround")
-    use("vim-scripts/ReplaceWithRegister")
+	-- tmux & split window navigation
+	use("christoomey/vim-tmux-navigator")
+	use("szw/vim-maximizer")
 
-    -- commenting with gc
-    use("numToStr/Comment.nvim")
+	-- essential plugins
+	use("tpope/vim-surround")
+	use("vim-scripts/ReplaceWithRegister")
 
-    -- file explorer
-    use("nvim-tree/nvim-tree.lua")
-    use("theprimeagen/harpoon")
+	-- commenting with gc
+	use("numToStr/Comment.nvim")
 
-    -- undo tree
-    use("sanfusu/neovim-undotree")
+	-- file explorer
+	use("nvim-tree/nvim-tree.lua")
+	use("theprimeagen/harpoon")
 
-    -- icons
-    use("kyazdani42/nvim-web-devicons")
+	-- undo tree
+	use("sanfusu/neovim-undotree")
 
-    --status line
-    use("nvim-lualine/lualine.nvim")
+	-- icons
+	use("kyazdani42/nvim-web-devicons")
 
-    -- fuzzy finding
-    use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
-    use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
- 
-    -- autocompletion
-    use("hrsh7th/nvim-cmp") -- completion plugin
-    use("hrsh7th/cmp-buffer") -- source for text in buffer
-    use("hrsh7th/cmp-path") -- source for file system paths
+	--status line
+	use("nvim-lualine/lualine.nvim")
+	-- fuzzy finding
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
+	use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
 
-    -- snippets
-    use("L3MON4D3/LuaSnip") -- snippet engine
-    use("saadparwaiz1/cmp_luasnip") -- for autocompletion
-    use("rafamadriz/friendly-snippets") -- useful snippets
+	-- autocompletion
+	use("hrsh7th/nvim-cmp") -- completion plugin
+	use("hrsh7th/cmp-buffer") -- source for text in buffer
+	use("hrsh7th/cmp-path") -- source for file system paths
 
-    -- markdown preview
-    use{ "iamcco/markdown-preview.nvim", run = "cd app && yarn install", cmd = "MarkdownPreview" }
+	-- snippets
+	use("L3MON4D3/LuaSnip") -- snippet engine
+	use("saadparwaiz1/cmp_luasnip") -- for autocompletion
+	use("rafamadriz/friendly-snippets") -- useful snippets
 
-    -- managing and installing lsp servers
-    use("williamboman/mason.nvim")
-    use("williamboman/mason-lspconfig.nvim")
+	-- markdown preview
+	use({ "iamcco/markdown-preview.nvim", run = "cd app && yarn install", cmd = "MarkdownPreview" })
 
-    -- configuring the lsp servers
-    use("neovim/nvim-lspconfig")
-    use("hrsh7th/cmp-nvim-lsp")
-    use({ "glepnir/lspsaga.nvim", branch = "main" })
-    use("onsails/lspkind.nvim")
+	-- managing and installing lsp servers, linters & formatters
+	use("williamboman/mason.nvim")
+	use("williamboman/mason-lspconfig.nvim")
 
-    if packer_bootstrap then
-        require("packer").sync()
-    end
+	-- configuring the lsp servers
+	use("neovim/nvim-lspconfig")
+	use("hrsh7th/cmp-nvim-lsp")
+	use({ "glepnir/lspsaga.nvim", branch = "main" })
+	use("onsails/lspkind.nvim")
+
+	-- formatting & linting
+	use("jose-elias-alvarez/null-ls.nvim")
+	use("jayp0521/mason-null-ls.nvim")
+
+	-- treesitter
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = function()
+			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+			ts_update()
+		end,
+	})
+
+	-- auto closing
+	use("windwp/nvim-autopairs")
+	use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" })
+
+	-- git signs plugin
+	use("lewis6991/gitsigns.nvim")
+
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
-
--- nvim/.config/nvim/lua/raphaelweis/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/raphaelweis/plugins/lsp/mason.lua
@@ -10,6 +10,12 @@ if not mason_lspconfig_status then
     return
 end
 
+-- import mason-null-ls plugin safely
+local mason_null_ls_status, mason_null_ls = pcall(require, "mason-null-ls")
+if not mason_null_ls_status then
+  return
+end
+
 -- enable mason
 mason.setup()
 
@@ -17,6 +23,13 @@ mason_lspconfig.setup({
     -- list of servers for mason to install
     ensure_installed = {
         "sumneko_lua",
+    }
+})
+
+mason_null_ls.setup({
+    ensure_installed = {
+        "prettier",
+        "stylua",
     }
 })
 
