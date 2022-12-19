4d3502f9b3eefeb9891283bf3253405659db0a28
Raphël Weis <raphael.weis.2003@gmail.com>
2022-12-19 17:24:46 +0100

    added comments to the init.lua file

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,7 +1,12 @@
+-- The plugins installation file
 require("raphaelweis.plugins-setup")
+
+-- The core folder with the basic neovim configuration
 require("raphaelweis.core.options")
 require("raphaelweis.core.keymaps")
 require("raphaelweis.core.colorscheme")
+
+-- The config files for the plugins that require it
 require("raphaelweis.plugins.comment")
 require("raphaelweis.plugins.nvim-tree")
 require("raphaelweis.plugins.lualine")
@@ -9,10 +14,10 @@ require("raphaelweis.plugins.telescope")
 require("raphaelweis.plugins.harpoon")
 require("raphaelweis.plugins.undotree")
 require("raphaelweis.plugins.nvim-cmp")
-require("raphaelweis.plugins.lsp.mason")
-require("raphaelweis.plugins.lsp.lspsaga")
-require("raphaelweis.plugins.lsp.lspconfig")
-require("raphaelweis.plugins.lsp.null-ls")
+require("raphaelweis.plugins.lsp.mason") --lsp
+require("raphaelweis.plugins.lsp.lspsaga") --lsp
+require("raphaelweis.plugins.lsp.lspconfig") --lsp
+require("raphaelweis.plugins.lsp.null-ls") --lsp
 require("raphaelweis.plugins.autopairs")
 require("raphaelweis.plugins.treesitter")
 require("raphaelweis.plugins.gitsigns")
