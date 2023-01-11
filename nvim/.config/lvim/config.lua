4670eb3fc03cf230b5b4af44d524028315b5e1a4
Raphaël Weis <raphael.weis.2003@gmail.com>
2023-01-11 13:17:07 +0100

    added a keymap to lvim config

--- nvim/.config/lvim/config.lua
+++ nvim/.config/lvim/config.lua
@@ -12,7 +12,10 @@ an executable
 lvim.log.level = "warn"
 lvim.format_on_save.enabled = false
 lvim.colorscheme = "lunar"
+
+-- User Config
 vim.opt.shiftwidth = 4
+
 --to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 
@@ -20,6 +23,8 @@ vim.opt.shiftwidth = 4
 lvim.leader = "space"
 -- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+
+vim.keymap.set("i", "jk", "<ESC>")
 -- lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 -- lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 -- unmap a default keymapping
@@ -61,7 +66,7 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 --   w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
 -- }
 
--- TODO: User Config for predefined plugins
+-- User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
