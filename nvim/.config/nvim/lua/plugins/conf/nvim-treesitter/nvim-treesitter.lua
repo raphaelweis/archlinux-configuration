6d4e4f9c0c142deb10e63dc27c5165a96219bbcc
Raphaël Weis <raphael.weis.2003@gmail.com>
2023-03-14 08:42:26 +0100

    changes

--- nvim/.config/nvim/lua/plugins/conf/nvim-treesitter/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/conf/nvim-treesitter/nvim-treesitter.lua
@@ -12,7 +12,14 @@ require("nvim-treesitter.configs").setup({
 		"markdown",
 		"markdown_inline",
 		"help",
+		"hocon",
 	}, -- ensure these treesitter parsers are installed
 	highlight = { enable = true }, -- enable better syntax highlighting
 	autotag = { enable = true }, -- automatic closing for html tags
 })
+
+local hocon_group = vim.api.nvim_create_augroup("hocon", { clear = true }) -- recognize *.conf files for hocon parser
+vim.api.nvim_create_autocmd(
+	{ "BufNewFile", "BufRead" },
+	{ group = hocon_group, pattern = "*/resources/*.conf", command = "set ft=hocon" }
+)
