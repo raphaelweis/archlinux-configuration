7cdfb904dd993eb875213a714d5052850a79c846
Raphaël Weis <raphael.weis.2003@gmail.com>
2023-03-14 11:53:07 +0100

    Revert "added auto reload of waybar (complicated)"
    This reverts commit 600d80d654ecfc1afc5c7dca6e35056c831e9d05.
    
    revert

--- nvim/.config/nvim/lua/misc/misc.lua
+++ nvim/.config/nvim/lua/misc/misc.lua
@@ -1,24 +1,10 @@
------------------------------
--- MISC
------------------------------
-
--- Auto reload waybar if a file is changed in the config directory, even if the config directory is a symling due to dotfile management
-
--- Get the path to the configuration directory
-local config_path = os.getenv("HOME") .. "/.config/waybar"
-
--- Try to resolve the symlink
-local symlink_path = io.open(config_path):read("*l")
-local resolved_path = symlink_path and require("lfs").attributes(symlink_path, "ino") or config_path
-
-if resolved_path then
-	-- Set up the file watcher
-	local uv = require("luv")
-	local watcher = uv.new_fs_event()
-	watcher:start(resolved_path, { recursive = true }, function()
-		-- Reload Waybar when the file changes
-		os.execute("killall -SIGUSR2 waybar")
-	end)
-else
-	print("Could not resolve symlink, using default path")
-end
+-- auto cmd
+vim.api.nvim_exec(
+	[[
+  augroup waybar_reload
+    autocmd!
+    autocmd BufWritePost $HOME/.config/waybar/* silent !killall -SIGUSR2 waybar
+  augroup END
+]],
+	true
+)
