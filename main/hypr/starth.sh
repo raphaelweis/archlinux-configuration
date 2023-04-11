ec90a71afb49f1bbe8f45e0e918ef30d3b4ec99d
Raphaël Weis <raphael.weis.2003@gmail.com>
2023-04-11 08:58:36 +0200

    added test for Hyprland process in startup script

--- main/hypr/starth.sh
+++ main/hypr/starth.sh
@@ -1,1 +1,9 @@
-exec Hyprland
+#!/bin/zsh
+
+ps aux | grep Hyprland | grep -v grep &> /dev/null #check if Hyprland process is running then redirect stdout and stderr to null output
+if ! [[ $? ]]
+then
+  exec Hyprland
+else
+  echo "Hyprland is already running !"
+fi
