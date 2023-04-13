813b5434fde9f1febac7a877afdd8d6b1a10caf5
Raphaël Weis <raphael.weis.2003@gmail.com>
2023-04-13 12:01:42 +0200

    added httpd.conf as a special file

--- stowRoot.sh
+++ stowRoot.sh
@@ -2,3 +2,4 @@
 
 # stow global stow packages, that require root privileges to run
 stow -d ./special wrappedhl_desktop -t /usr/share/wayland-sessions/
+stow -d ./special httpd_conf -t /opt/lampp/etc/
