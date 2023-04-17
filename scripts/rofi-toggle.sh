dca75e2320d93393785045f21ddf4697f568ffcc
Raphaël Weis <raphael.weis.2003@gmail.com>
2023-04-17 21:08:57 +0200

    rofi script is not working

--- scripts/rofi-toggle.sh
+++ scripts/rofi-toggle.sh
@@ -3,13 +3,13 @@
 mode=$1
 
 if [[ -z $mode ]]; then
-    mode="run"
+  mode="run"
 fi
 
-if pgrep -x "rofi" &> /dev/null; then
-    # If rofi is running, close it
-    pkill rofi
+if pgrep -x "rofi"; then
+  # If rofi is running, close it
+  pkill -x "rofi"
 else
-    # If rofi is not running, launch it in the specified mode
-    rofi -show $mode &> /dev/null &
+  # If rofi is not running, launch it in the specified mode
+  (rofi -show run &> /dev/null) &
 fi
