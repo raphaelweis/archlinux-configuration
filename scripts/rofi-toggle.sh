eb49e9cd755564f2d5e49c308daacb524e94eb86
Raphaël Weis <raphael.weis.2003@gmail.com>
2023-04-17 20:11:29 +0200

    redirected error output

--- scripts/rofi-toggle.sh
+++ scripts/rofi-toggle.sh
@@ -6,10 +6,10 @@ if [[ -z $mode ]]; then
     mode="run"
 fi
 
-if pgrep -x "rofi" > /dev/null; then
+if pgrep -x "rofi" &> /dev/null; then
     # If rofi is running, close it
     pkill rofi
 else
     # If rofi is not running, launch it in the specified mode
-    rofi -show $mode &
+    rofi -show $mode &> /dev/null &
 fi
