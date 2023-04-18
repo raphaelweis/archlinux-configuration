96057a4ddee76a813d20df3c90d8e2bed8f2cbe2
Raphaël Weis <raphael.weis.2003@gmail.com>
2023-04-18 22:30:31 +0200

    wallpaper picker

--- scripts/wallpaper-picker.sh
+++ scripts/wallpaper-picker.sh
@@ -1,12 +1,9 @@
-#!/bin/bash
-
 DEFAULT_WALLPAPER="$WALLPAPERS_DIR/default/wall"
 
 # pick a wallpaper with rofi
 chosen=$(ls ~/Pictures/wallpapers | rofi -dmenu -i)
 
 # copy chosen wallpaper to the default wallpaper directory and rename it to wall
-rm "$DEFAULT_WALLPAPER"
 cp "$WALLPAPERS_DIR/$chosen" "$DEFAULT_WALLPAPER"
 
 # select default wallpaper as current one with swww
