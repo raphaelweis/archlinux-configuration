16706bb5281be471e989df8962d52b6e033e7f5d
Raphaël Weis <raphael.weis.2003@gmail.com>
2023-04-20 01:26:27 +0200

    mainly notification configuration + new icons

--- scripts/record.sh
+++ scripts/record.sh
@@ -10,9 +10,11 @@ if ! pkill -INT -P "$(pgrep -xo record.sh)" wf-recorder 2>/dev/null; then
 
     pkill -USR1 -F "/tmp/record-widget.pid"
 
+    notify-send "Recording started" "recording without audio - mp4 format" -i "$XDG_CONFIG_HOME/dunst/icons/recording-icon-color.png"
     recording="$RECORDINGS_DIR/screen-record-$(date +%Y-%m-%d-%H-%M-%S).mp4"
     wf-recorder -f "$recording" -g "$geometry"
 
+    notify-send "Recording stopped" "the recording has been saved to<br>$recording" -i "$XDG_CONFIG_HOME/dunst/icons/new-video-icon-black.png"
     pkill -USR2 -F "/tmp/record-widget.pid"
   fi
 else
--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -3,5 +3,6 @@
 SCREENSHOTS_DIR="$HOME/Pictures/screenshots"
 
 screenshot="$SCREENSHOTS_DIR/screenshot-$(date +%Y-%m-%d-%H-%M-%S).png"
-grim -g "$(slurp)" "$screenshot"
+grim -g "$(slurp -d)" "$screenshot"
 wl-copy < "$screenshot"
+notify-send "New screenshot" "the screenshot has been saved to<br>$screenshot" -i "$XDG_CONFIG_HOME/dunst/icons/screenshot-icon-color.png"
