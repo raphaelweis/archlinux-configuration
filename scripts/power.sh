e10246cbdccb3c3ff2a0f7e6d776a7bc1d257e13
Raphaël Weis <raphael.weis.2003@gmail.com>
2023-04-19 23:53:43 +0200

    added additionnal configuration for rofi powermenu

--- scripts/power.sh
+++ scripts/power.sh
@@ -1,6 +1,6 @@
-#! /bin/bash
+#!/bin/bash
 
-chosen=$(printf "  Power Off\n  Reboot" | rofi -dmenu -i)
+chosen=$(printf "  Power Off\n  Reboot" | rofi -dmenu -i -theme-str '@import "power.rasi"')
 
 case "$chosen" in
 	"  Power Off") poweroff ;;
