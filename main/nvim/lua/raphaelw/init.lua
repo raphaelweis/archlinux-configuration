413d66e3fda8389cd0d6927ebf59d3a3b3afc764
Raphaël Weis <raphael.weis.2003@gmail.com>
2023-04-22 15:14:19 +0200

    fixed ftplugin folder organization

--- main/nvim/lua/raphaelw/init.lua
+++ main/nvim/lua/raphaelw/init.lua
@@ -4,9 +4,3 @@ require("raphaelw.core.options")
 
 -- plugins
 require("raphaelw.plugins.plugins-declaration")
-
--- filetype files
-require("raphaelw.ftplugin.json")
-require("raphaelw.ftplugin.jsonc")
-require("raphaelw.ftplugin.markdown")
-require("raphaelw.ftplugin.tex")
